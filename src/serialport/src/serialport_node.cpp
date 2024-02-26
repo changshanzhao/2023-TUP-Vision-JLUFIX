1cd2c64a1455cc42aadc349e7874f7b02f895264
changshanzhao <qq2395624242@163.com>
2024-02-26 19:12:00 +0800

    1

--- src/serialport/src/serialport_node.cpp
+++ src/serialport/src/serialport_node.cpp
@@ -278,8 +278,8 @@ namespace serialport
                 vision_data = 
                 {
                     (serial_port_->steady_clock_.now().nanoseconds() / 1e6),
-                    0.8*(float)target_info->pitch + (float)target_info->imu_pitch/3.1415926535*180, 
-                    -(0.8*(float)target_info->yaw + (float)target_info->imu_yaw/3.1415926535*180), 
+                    (float)target_info->pitch + (float)target_info->imu_pitch/3.1415926535*180, 
+                    -((float)target_info->yaw + (float)target_info->imu_yaw/3.1415926535*180), 
                     (float)target_info->distance, 
                     (target_info->is_switched || target_info->is_spinning_switched), 
                     target_info->is_target, 
